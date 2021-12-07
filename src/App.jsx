effb79c8a65f43ae7b3f21e4ba7442a35264f21f
geko <geko@MacBook-Pro-de-geko.local>
2021-12-07 18:57:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <h1 className="text-4xl">This is a react + vite + tailwind template</h1>
+    </div>
+  );
+}
+
+export default App;
